ecad4c1d9c9f233760eca4a7c1dfc3a08c7b20ce
Chris Stiteler <chris@embed.ly>
2016-01-31 13:54:27 -0500

    serves static files and sets up dashboard template

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+// serve static assignment directory
+app.use(express.static('assignment'));
+
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
