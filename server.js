c2fcee82dace1e2f7fa2401601cba85dce48797d
Chris Stiteler <chris@embed.ly>
2016-02-05 18:10:02 -0500

    minor fixes

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-// serve static assignment directory
-app.use(express.static('public'));
-
 // quick routes for static pages
 app.get('/register', function(req, res){
     res.sendFile(__dirname + '/public/assignment/register.html')
@@ -34,5 +31,7 @@ app.get('/fields', function(req, res){
 });
 // end routes for static pages
 
+// serve static assignment directory
+app.use(express.static('public'));
 
 app.listen(port, ipaddress);
