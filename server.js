0030774e15cea146ce1141e517aa187428883a92
Chris Stiteler <chris@embed.ly>
2016-02-05 19:02:27 -0500

    testing with openshift directory envs

--- server.js
+++ server.js
@@ -4,30 +4,39 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+if (typeof process.env.OPENSHIFT_HOMEDIR != 'undefined') {
+  console.log("openshift environment detected");
+  dir = process.env.OPENSHIFT_HOMEDIR;
+} else {
+  console.log("local environment detected");
+  dir = __dirname
+}
+
+
 // quick routes for static pages
 app.get('/register', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/register.html')
+    res.sendFile(dir + '/public/assignment/register.html')
 });
 app.get('/login', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/login.html')
+    res.sendFile(dir + '/public/assignment/login.html')
 });
 app.get('/forms', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/forms.html')
+    res.sendFile(dir + '/public/assignment/forms.html')
 });
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/home.html')
+    res.sendFile(dir + '/public/assignment/home.html')
 });
 app.get('/profile', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/profile.html')
+    res.sendFile(dir + '/public/assignment/profile.html')
 });
 app.get('/admin', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/admin.html')
+    res.sendFile(dir + '/public/assignment/admin.html')
 });
 app.get('/logout', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/home.html')
+    res.sendFile(dir + '/public/assignment/home.html')
 });
 app.get('/fields', function(req, res){
-    res.sendFile(__dirname + '/public/assignment/fields.html')
+    res.sendFile(dir + '/public/assignment/fields.html')
 });
 // end routes for static pages
 
