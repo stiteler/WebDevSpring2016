7e4d2973fcaf2cf7d5fbd007e11a28fb196772c9
Chris Stiteler <chris@embed.ly>
2016-03-10 18:43:41 -0500

    adds admin

---
+++ public/project/client/views/admin/admin.controller.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('FlairdropApp')
+        .controller('AdminController', AdminController);
+
+    function AdminController($scope, $rootScope, UserService) {
+        var vm = this;
+
+        function init() {
+            vm.$rootScope = $rootScope;
+            vm.$scope = $scope;
+            vm.UserService = UserService;
+
+            // models:
+            UserService.findAllUsers(function(result) {
+                console.log(result);
+                vm.users = result;
+            });
+
+            // api key: b16d33bed47a4b61a4ce19e8e2b7f039
+
+      }
+      init();
+    }
+}());
