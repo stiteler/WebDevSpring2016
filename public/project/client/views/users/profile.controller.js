80f18fa30a1de7733a082aad36c8cbfd5f2f4147
Chris Stiteler <cstiteler@comcast.net>
2016-03-11 23:54:25 -0500

    final changes for PoC

--- public/project/client/views/users/profile.controller.js
+++ public/project/client/views/users/profile.controller.js
@@ -7,30 +7,61 @@
 
     function ProfileController($sce, $scope, $rootScope, RecommendService, UserService, EmbedlyService) {
         var vm = this;
+        var us = UserService;
+        var rs = RecommendService;
+
+        vm.getRecosForFlair = getRecosForFlair;
+        vm.connect = connect;
 
         function init() {
             // vm.sessionUser = $rootScope.user;
             // just for PoC I'm setting this to be Alice.
-            UserService.getUserByUserId(123, function(user) {
+            us.getUserByUserId(123, function(user) {
                 vm.profile = user;
             });
-
-            _updateMedia();
-            
+            _updateModels();
         }
         init();
+
+        // just as a PoC this needs to use the ConnectionService
+        function connect() {
+            $rootScope.connected = true;
+            _updateModels();
+        }
+
+        function getRecosForFlair(flair) {
+            var these = [];
+            for (var i in vm.recos) {
+                if(vm.recos[i]) {
+                    var obj = vm.recos[i];
+                    if(vm.recos[i].flair == flair) {
+                        these.push(obj);
+                    }
+                }
+            }
+            return these;
+        }
+
+        function _updateModels() {
+            rs.getRecommendsForUser(vm.profile.username, function(recos) {
+                console.log(recos);
+                vm.recos = recos;
+            })
+            vm.connected = $rootScope.connected ? true : false;
+
+            _updateMedia();
+        }
 
         function _updateMedia() {
             // get the media embed for this user
             if (vm.profile.media) {
                 EmbedlyService.getEmbedByMediaUrl(vm.profile.media)
                 .then(function(oembed) {
-                    console.log("success: api_call");
+                    // console.log("success: api_call");
                     vm.profile.mediaIFrame = $sce.trustAsHtml(oembed.data.html);
-                    console.log(vm.profile);
                 },
                 function() {
-                    console.log("failure: api_call");
+                    // console.log("failure: api_call");
                     vm.profile.mediaIFrame = null;
                 });
             }
@@ -50,7 +81,7 @@
             var new_id = user_map[username];
             console.log('new id: ' + new_id)
             if (new_id) {
-                UserService.getUserByUserId(new_id, function(result) {
+                us.getUserByUserId(new_id, function(result) {
                     console.log(result);
                     vm.profile = result;
                     _updateMedia();
