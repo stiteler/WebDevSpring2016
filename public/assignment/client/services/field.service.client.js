e67ce3034c208102a1207a5ec29433176ed71b00
Chris Stiteler <cstiteler@comcast.net>
2016-03-17 01:13:48 -0400

    adds form/field services

---
+++ public/assignment/client/services/field.service.client.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('FormBuilderApp')
+        .factory('FieldService', FieldService);
+
+    function FieldService() {
+        var api = {
+            createFieldForForm: createFieldForForm,
+            getFieldsForForm: getFieldsForForm,
+            getFieldForForm: getFieldForForm,
+            deleteFieldFromForm, deleteFieldFromForm,
+            updateField: updateField,
+        };
+        return api;
+
+        function createFieldForForm(formId, field) {
+            return $http({
+                method: 'POST',
+                url: '/api/assignment/form/' + formId + 'field',
+                data: field
+            });
+        }
+
+        function getFieldsForForm(formId) {
+            return $http.get('/api/assignment/form/' + formId + field);
+        }
+
+        function getFieldForForm(formId, fieldId) {
+            return $http.get('/api/assignment/form/' + formId + '/field/' + fieldId);
+        }
+
+        function deleteFieldFromForm(formId, fieldId) {
+            $http.delete('/api/assignment/form/' + formId + '/field/' + fieldId);
+        }
+
+        function updateField(formId, fieldId, field) {
+            return $http({
+                method: 'PUT',
+                url: '/api/assignment/form/' + formId + '/field/' + fieldId,
+                data: field
+            });
+        }
+
+    }
+}());
--- public/assignment/client/services/forms.service.client.js
+++ public/assignment/client/services/forms.service.client.js
@@ -6,8 +6,6 @@
         .factory('FormService', FormService);
 
     function FormService() {
-        var forms = _getForms();
-
         var api = {
             createFormForUser: createFormForUser,
             findAllFormsForUser: findAllFormsForUser,
@@ -16,77 +14,28 @@
         };
         return api;
 
-        function createFormForUser(userId, form, callback) {
-            form._id = (new Date()).getTime();
-            form.userId = userId;
-            forms.push(form);
-            callback(_copyForm(form));
+        function createFormForUser(userId, form) {
+            return $http({
+                method: 'POST',
+                url: '/api/assignment/user/' + userId + 'form',
+                data: form
+            });
         }
 
-        function findAllFormsForUser(userId, callback) {
-            var result = _searchForms('userId', userId);
-            callback(result);
+        function findAllFormsForUser(userId) {
+            return $http.get('/api/assignment/user/' + userId + '/form');
         }
 
-        function deleteFormById(formId, callback) {
-            var matches = _searchForms('_id', formId);
-            if (matches.length > 0) {
-                // a form was found with that id.
-                var match = matches[0];
-                var index = forms.indexOf(match);
-                forms.splice(index, 1);
-            }
-            // this is a vuln. if alice can get bob's forms.
-            callback(forms);
+        function deleteFormById(formId) {
+            return $http.delete('/api/assignment/form/' + formId);
         }
 
-        function updateFormById(formId, newForm, callback) {
-            var matches = _searchForms('_id', formId);
-            if (matches.length > 0) {
-                var match = matches[0];
-                // for each update in newForm
-                // apply to match
-                for (var attr in newForm) {
-                    if (newForm[attr]) {
-                        match[attr] = newForm[attr];
-                    }
-                }
-                callback(_copyForm(match));
-            } else {
-                callback(null);
-            }
-        }
-
-        function _copyForm(form) {
-            var copy = {
-                _id: form._id,
-                title: form.title,
-                userId: form.userId,
-            };
-            return copy;
-        }
-
-        function _searchForms(attr, value) {
-            // returns forms where the given attr
-            // ie. userId or _id match value.
-            var rtn = [];
-            for (var ind in forms) {
-                if (forms[ind]) {
-                    var form = forms[ind];
-                    if (form[attr] === value) {
-                        rtn.push(form);
-                    }
-                }
-            }
-            return rtn;
-        }
-
-        function _getForms() {
-            return [
-              {_id: '000', title: 'Contacts', userId: 123},
-              {_id: '010', title: 'ToDo',     userId: 123},
-              {_id: '020', title: 'CDs',      userId: 234},
-            ];
+        function updateFormById(formId, newForm) {
+            return $http({
+                method: 'PUT',
+                url: '/api/assignment/form/' + formId,
+                data: newForm
+            });
         }
     }
 }());
