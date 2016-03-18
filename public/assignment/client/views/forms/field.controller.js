a6e8aa3b97bdd846818045a71f7cdc1ea9efd7f6
Chris Stiteler <cstiteler@comcast.net>
2016-03-17 23:39:14 -0400

    basics of clicking functionality

--- public/assignment/client/views/forms/field.controller.js
+++ public/assignment/client/views/forms/field.controller.js
@@ -5,13 +5,41 @@
         .module('FormBuilderApp')
         .controller('FieldController', FieldController);
 
-    function FieldController() {
+    function FieldController(UserService, UtilsService) {
         var model = this;
 
+        model.selectType = selectType;
+        model.addField = addField;
+
         function init() {
-            
+            if (UtilsService.isLoggedIn()) {
+                model.user = UserService.getCurrentUser();
+                model.fieldTypes = _getFieldTypes();
+                model.selectedFieldType = model.fieldTypes[0];
+            } else {
+                UtilsService.navigate('#/home');
+            }
         }
         init();
 
+        function addField() {
+            var type = model.selectedFieldType;
+            console.log('adding field: ' + type);
+        }
+
+        function selectType(type) {
+            model.selectedFieldType = type;
+        }
+
+        function _getFieldTypes() {
+            return [
+                'Single Line Text Field',
+                'Multi Line Text Field',
+                'Date Field',
+                'Dropdown Field',
+                'Checkboxes Field',
+                'Radio Buttons Field'
+            ];
+        }
     }
 }());
