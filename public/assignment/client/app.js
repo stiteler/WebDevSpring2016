3e2a3456c7c7dcd7488bb3df2766594e26cf074c
Chris Stiteler <cstiteler@comcast.net>
2016-03-18 03:57:47 -0400

    tablify's the fields and adds sortable/draggable directive

--- public/assignment/client/app.js
+++ public/assignment/client/app.js
@@ -3,7 +3,6 @@
 
     angular
         .module('FormBuilderApp', [
-            'ngRoute',
-            'jgaSortable'
+            'ngRoute'
         ]);
 })();
--- public/assignment/client/views/forms/field.controller.js
+++ public/assignment/client/views/forms/field.controller.js
@@ -3,6 +3,34 @@
 
     angular
         .module('FormBuilderApp')
+        .directive('jgaSortable', function() {
+            console.log('in directive');
+
+            var start = null;
+            var end = null;
+
+            function link(scope, element, attributes) {
+                var jgaAxis = attributes.jgaAxis;
+                $(element).sortable({
+                    axis: jgaAxis,
+                    start: function(event, ui) {
+                        start = ui.item.index();
+                    },
+                    stop: function(event, ui) {
+                        end = ui.item.index();
+                        var fields = scope.model.form.fields;
+                        var temp = fields[start];
+                        fields[start] = fields[end];
+                        fields[end] = temp;
+                        scope.$apply();
+                    }
+                });
+            }
+
+            return {
+                link: link
+            };
+        })
         .controller('FieldController', FieldController);
 
     function FieldController(UserService, UtilsService) {
