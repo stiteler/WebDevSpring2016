9583d9643408d9b8909c133b7d997e8c229628a7
Chris Stiteler <cstiteler@comcast.net>
2016-03-18 01:29:41 -0400

    forms rendering dynamically with test data

--- public/assignment/client/views/forms/field.controller.js
+++ public/assignment/client/views/forms/field.controller.js
@@ -14,6 +14,9 @@
         function init() {
             if (UtilsService.isLoggedIn()) {
                 model.user = UserService.getCurrentUser();
+                model.form = _getTestForm();
+                model.fields = model.form.fields;
+
                 model.fieldTypes = _getFieldTypes();
                 model.selectedFieldType = model.fieldTypes[0];
             } else {
@@ -40,6 +43,89 @@
                 'Checkboxes Field',
                 'Radio Buttons Field'
             ];
+        }
+
+        function _fieldMappings() {
+            return {
+                'Single Line Text Field': 'TEXT',
+                'Multi Line Text Field': 'TEXTAREA',
+                'Date Field': 'DATE',
+                'Dropdown Field': 'OPTIONS',
+                'Checkboxes Field': 'CHECKBOXES',
+                'Radio Buttons Field': 'RADIOS'
+            };
+        }
+
+        function _getTestForm() {
+           return {
+              "_id": "010",
+              "title": "ToDo",
+              "userId": 234,
+              "fields":[
+                 {
+                    "_id":"777",
+                    "label":"Title",
+                    "type":"TEXT",
+                    "placeholder":"Title"
+                 },
+                 {
+                    "_id":"888",
+                    "label":"Description",
+                    "type":"TEXTAREA",
+                    "placeholder":"Title"
+                 },
+                 {
+                    "_id":"999",
+                    "label":"Due Date",
+                    "type":"DATE"
+                 },
+                 {
+                    "_id":"145",
+                    "label":"State",
+                    "type":"OPTIONS",
+                    "options":[
+                       {
+                          "label":"Massachussetts",
+                          "value":"MA"
+                       },
+                       {
+                          "label":"New Hampshire",
+                          "value":"NH"
+                       }
+                    ]
+                 },
+                 {
+                    "_id":"444",
+                    "label":"Sex/Gender",
+                    "type":"RADIOS",
+                    "options":[
+                       {
+                          "label":"Male",
+                          "value":"Male"
+                       },
+                       {
+                          "label":"Female",
+                          "value":"Female"
+                       }
+                    ]
+                 },
+                 {
+                    "_id":"345",
+                    "label":"Moods",
+                    "type":"CHECKBOXES",
+                    "options":[
+                       {
+                          "label":"Happy",
+                          "value":"happy"
+                       },
+                       {
+                          "label":"Accomplished",
+                          "value":"accomplished"
+                       }
+                    ]
+                 }
+              ]
+           };
         }
     }
 }());
