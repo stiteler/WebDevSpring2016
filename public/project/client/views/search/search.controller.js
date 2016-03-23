feea86648a310608ea693bca2854e9011adde6bf
Chris Stiteler <cstiteler@comcast.net>
2016-03-23 01:57:21 -0400

    adds new search page templating

---
+++ public/project/client/views/search/search.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('FlairdropApp')
+        .controller('SearchController', SearchController);
+
+    function SearchController($sce, RecommendService, UserService) {
+        var model = this;
+        
+    }
+}());
