75c9e190fc92aacc088ee70ea30a036faa7f2504
Chris Stiteler <chris@embed.ly>
2016-03-23 19:34:22 -0400

    impls some login functionality

--- public/project/client/views/users/login.controller.js
+++ public/project/client/views/users/login.controller.js
@@ -5,26 +5,37 @@
         .module('FlairdropApp')
         .controller('LoginController', LoginController);
 
-    function LoginController(UserService) {
+    function LoginController(UserService, $location) {
         var model = this;
         model.login = login;
         model.forgot = forgot;
+
+        function init() {
+            console.log("Login Init");
+        }
+        init();
 
         function forgot() {
             console.log('forgot password!');
         }
 
         function login() {
-            var user = null;
-            var un = model.username;
-            var pw = model.password;
+            console.log('clicked login');
+            UserService.findUserByCredentials(model.username, model.password, function(user) {
+                if(user) {
+                    UserService.login(user._id, function() {
+                        console.log("logged in");
+                    });
+                    $location.path('/profile');
+                }
+            });
 
             // UserService
             //     .findUserByCredentials(un, pw)
             //     .then(function(resp) {
             //         if (resp.data) {
             //             console.log(resp.data);
-            //             UserService.setCurrentUser(resp.data);
+            //             UserService.login(resp.data);
             //             UtilsService.navigate('/profile');
             //         }
             //     });
