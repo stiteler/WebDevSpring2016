1c7ee794ba88bbb10f67937667816bb2d34dfdc4
Chris Stiteler <cstiteler@comcast.net>
2016-03-24 22:28:26 -0400

    implements client side register controller/view

--- public/project/client/views/users/register.controller.js
+++ public/project/client/views/users/register.controller.js
@@ -5,8 +5,36 @@
         .module('FlairdropApp')
         .controller('RegisterController', RegisterController);
 
-    function RegisterController(UserService) {
+    function RegisterController(UserService, $location) {
         var model = this;
+        model.register = register;
         
+        function register() {
+            // eventually will be checking on server side.
+            if (model.password != model.confirm) {
+                model.warningMessage = "Passwords must match."
+                model.password = null;
+                model.confirm = null;
+                return;
+            }
+
+            var newUser = {
+                username: model.username,
+                password: model.password,
+                email: model.email,
+                firstName: '',
+                lastName: ''
+            };
+
+            UserService.createUser(newUser)
+                .then(function (created) {
+                    if (created.data) {
+                        UserService.setCurrentUser(created.data);
+                        $location.path('/profile');
+                    }
+                }, function(err) {
+                    model.errorMessage = err.data;
+                });
+        }
     }
 }());
