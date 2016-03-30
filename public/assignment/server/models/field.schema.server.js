d19b0c1b1763f004ec08272d49ef25cc60eafeef
Chris Stiteler <cstiteler@comcast.net>
2016-03-29 23:11:09 -0400

    adds assignment schemas

---
+++ public/assignment/server/models/field.schema.server.js
@@ -0,0 +1,12 @@
+module.exports = function(mongoose) {
+
+    var FieldSchema = mongoose.Schema({
+	label: String,
+	type: {type: String, default: 'TEXT'},
+	placeholder: String,
+	options: [{label: String, value: String}]
+    }, {collection: 'assignment-field'});
+
+    return FieldSchema;
+
+};
---
+++ public/assignment/server/models/user.schema.server.js
@@ -0,0 +1,14 @@
+module.exports = function(mongoose) {
+
+    var UserSchema = mongoose.Schema({
+        username: String,
+        password: String,
+        firstName: String,
+        lastName: String,
+        emails: [String],
+        phones: [String],
+    }, {collection: 'assignment-user'});
+
+    return UserSchema;
+
+};
