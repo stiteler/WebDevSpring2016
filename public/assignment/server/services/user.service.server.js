15d78cbdd810b13acd467babcc03265a81ee6e94
Chris Stiteler <cstiteler@comcast.net>
2016-04-09 17:49:32 -0400

    adapting user service for passportjs

--- public/assignment/server/services/user.service.server.js
+++ public/assignment/server/services/user.service.server.js
@@ -1,9 +1,118 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require("bcrypt-nodejs");
+
 module.exports = function(app, UserModel) {
     app.post('/api/assignment/user', createUser);
     app.get('/api/assignment/user', getUser);
     app.get('/api/assignment/user/:id', getUserById);
     app.put('/api/assignment/user/:id', updateUser);
     app.delete('/api/assignment/user/:id', deleteUser);
+
+    // auth endpoints
+    var auth = authorized;
+    app.post('/api/assignment/login', passport.authenticate('local'), login);
+    app.post('/api/assignment/logout', logout);
+    app.post('/api/assignment/register', register);
+    app.get('/api/assignment/loggedin', loggedin);
+
+
+    passport.use(new LocalStrategy(localStrategy));
+    passport.serializeUser(serializeUser);
+    passport.deserializeUser(deserializeUser);
+
+    function localStrategy(username, password, done) {
+        UserModel
+            .findUserByCredentials({username: username, password: password})
+            .then(
+                function(user) {
+                    if (!user) { return done(null, false); }
+                    return done(null, user);
+                },
+                function(err) {
+                    if (err) { return done(err); }
+                }
+            );
+    }
+
+    function serializeUser(user, done) {
+        done(null, user);
+    }
+
+    function deserializeUser(user, done) {
+        UserModel
+            .findUserById(user._id)
+            .then(
+                function(user){
+                    done(null, user);
+                },
+                function(err){
+                    done(err, null);
+                }
+            );
+    }
+
+    function authorized (req, res, next) {
+        if (!req.isAuthenticated()) {
+            res.send(401);
+        } else {
+            next();
+        }
+    };
+
+    function loggedin(req, res) {
+        res.send(req.isAuthenticated() ? req.user : '0');
+    }
+
+    function logout(req, res) {
+        req.logOut();
+        res.send(200);
+    }
+
+    function login(req, res) {
+        var user = req.user;
+        delete user.password;
+        res.json(user);
+    }
+
+
+    // Model CRUD
+    function register (req, res) {
+        var newUser = req.body;
+        UserModel
+            .finduserByUsername(newUser.username)
+            .then(
+                function(user){
+                    if(user) {
+                        res.json(null);
+                    } else {
+                        // encrypt the password when registering
+                        user.password = bcrypt.hashSync(newUser.password);
+                        return userModel.createDeveloper(developer);
+                    }
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+            .then(
+                function(user){
+                    if(user){
+                        req.login(user, function(err) {
+                            if(err) {
+                                res.status(400).send(err);
+                            } else {
+                                res.json(user);
+                            }
+                        });
+                    }
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            );
+    }
+
 
     function updateUser(req, res) {
         var uid = req.params.id;
@@ -44,19 +153,19 @@ module.exports = function(app, UserModel) {
         }
     }
 
-    function login(req, res) {
-        var username = req.query.username;
-        var password = req.query.password;
+    // function login(req, res) {
+    //     var username = req.query.username;
+    //     var password = req.query.password;
 
-        UserModel
-            .findUserByCredentials(username, password)
-            .then(function(user) {
-                console.log(user);
-                res.status(200).json(user);
-            }, function(err) {
-                res.status(403).json(err);
-            });
-    }
+    //     UserModel
+    //         .findUserByCredentials(username, password)
+    //         .then(function(user) {
+    //             console.log(user);
+    //             res.status(200).json(user);
+    //         }, function(err) {
+    //             res.status(403).json(err);
+    //         });
+    // }
 
     function getUserByUsername(req, res) {
         var username = req.query.username;
